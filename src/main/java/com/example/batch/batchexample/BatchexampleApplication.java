f89f3cb7bdf26bb778a98bc6cc345788b69e9d41
criscagi <cristian.castano@ceiba.com.co>
2020-10-07 20:49:42 -0500

    se crea la arquitectura del proyecto

---
+++ src/main/java/com/example/batch/batchexample/BatchexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batch.batchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/batch/batchexample/BatchexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch.batchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
